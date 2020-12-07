2cc821acf95fd9377c8c9b5f0548b845c712bdc4
Nick Marsh <nick@getfu.com.au>
2020-12-07 12:53:30 +1000

    Day 6

--- ts/src/day6/index.ts
+++ ts/src/day6/index.ts
@@ -1,4 +1,31 @@
-import { readLines } from '../lib';
+import { readLinesTemp } from '../lib';
 
-const input = readLines('src/day6/sample.txt');
+const input = readLinesTemp('src/day6/input.txt');
 console.log('input', input);
+
+let sumOfCountsPt1 = 0;
+let sumOfCountsPt2 = 0;
+for (const group of input) {
+  const joined = group.join('');
+  console.log(`group joined: ${joined}`);
+
+  const answersMap = new Map();
+  for (const char of joined) {
+    const curCount = answersMap.has(char) ? answersMap.get(char) : 0;
+    answersMap.set(char, curCount + 1);
+  }
+  console.log(`group answers: ${answersMap.size}`);
+  sumOfCountsPt1 += answersMap.size;
+
+  let allAnswered = 0;
+  answersMap.forEach((v: number) => {
+    if (v === group.length) {
+      allAnswered++;
+    }
+  });
+  sumOfCountsPt2 += allAnswered;
+}
+
+console.log(`Part 1: ${sumOfCountsPt1}`);
+console.log(`Part 2: ${sumOfCountsPt2}`);
+
--- ts/src/lib/index.ts
+++ ts/src/lib/index.ts
@@ -2,3 +2,21 @@ import * as fs from 'fs';
 
 export const readLines = (path: string) =>
   fs.readFileSync(path, 'utf8').split('\n').map((line: string) => line.trim());
+
+export const readLinesTemp = (path: string) => {
+  const lines = readLines(path);
+
+  const groups: string[][] = [];
+  let curGroup: string[] = [];
+  lines.forEach((line) => {
+    if (line === '') {
+      groups.push(curGroup);
+      curGroup = [];
+    } else {
+      curGroup.push(line);
+    }
+  });
+  groups.push(curGroup);
+
+  return groups;
+}
