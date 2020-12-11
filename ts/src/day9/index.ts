82a486009498b249ccdb28ab56fbe7d4631c62d6
Nick Marsh <nick@getfu.com.au>
2020-12-11 11:45:47 +1000

    Day 9

--- ts/src/day9/index.ts
+++ ts/src/day9/index.ts
@@ -1,69 +1,122 @@
 import { readLinesAsInts } from '../lib';
 
-const input = readLinesAsInts('src/day9/sample.txt');
+const input = readLinesAsInts('src/day9/input.txt');
 // console.log('input', input);
 
-interface Entry {
-    num: number;
-    sums: Map<number, number>;
+class Entry {
+    private readonly num: number;
+    private sumsList: number[];
+
+    constructor(num: number) {
+        this.num = num;
+        this.sumsList = [];
+    }
+
+    get number() {
+        return this.num;
+    }
+
+    addNumber(otherNum: number) {
+        this.sumsList.push(this.num + otherNum);
+    }
+
+    hasSum(sum: number) {
+        return this.sumsList.some((n: number) => n === sum);
+    }
+
+    removeFirstSum() {
+        if (this.sumsList.length < 1) {
+            return;
+        }
+        this.sumsList.unshift();
+    }
 }
 
-const entries: Entry[] = [];
-const preambleSize = 5;
+// tslint:disable-next-line:max-classes-per-file
+class Entries {
+    private readonly preambleSize: number;
+    private entriesList: Entry[] = [];
 
-for (let i = 0; i < preambleSize; i++) {
-    const curNum = input[i];
-    const entry = { num: curNum, sums: new Map<number, number>() };
-    for (let j = 0; j < preambleSize; j++) {
-        if (i !== j) {
-            const otherNum = input[j];
-            const sum = curNum + otherNum;
-            if (entry.sums.has(sum)) {
-                console.error('duplicate sum', { entry, sum, otherNum });
-            }
-            entry.sums.set(sum, otherNum);
+    constructor(preambleSize: number) {
+        this.preambleSize = preambleSize;
+    }
+
+    processNumber(num: number): boolean {
+        if (this.entriesList.length < this.preambleSize) {
+            this.addToPreamble(num);
+            return true;
+        } else {
+            return this.checkSum(num);
         }
     }
-    entries.push(entry);
-}
 
-input.splice(0, preambleSize);
+    private addToPreamble(num: number) {
+        console.log('adding number to preamble', { num });
 
-// console.log('entries', entries);
-// console.log('input', input);
-
-const findSum = (num: number): number => {
-    for (let i = 0; i < entries.length; i++) {
-        const entry = entries[i];
-        if (entry.sums.has(num)) {
-            return i;
+        const newEntry = new Entry(num);
+        for (const entry of this.entriesList) {
+            entry.addNumber(num);
+            newEntry.addNumber(entry.number);
         }
+        this.entriesList.push(newEntry);
     }
-    return -1;
+
+    private checkSum(num: number): boolean {
+        console.log('searching for sum', { num });
+
+        if (this.entriesList.every(entry => !entry.hasSum(num))) {
+            return false;
+        }
+
+        this.entriesList.shift();
+        this.entriesList.forEach(entry => entry.removeFirstSum());
+        this.addToPreamble(num);
+
+        return true;
+    }
 }
 
-while (input.length > 0) {
-    const curNum = input.shift();
-    if (curNum === undefined) {
-        break;
-    }
+const entries = new Entries(25);
+let badNum = undefined;
 
-    const sumIndex = findSum(curNum);
-    if (sumIndex === -1) {
+for (const curNum of input) {
+    const ok = entries.processNumber(curNum);
+    if (!ok) {
+        badNum = curNum;
         console.log(`Part 1: ${curNum}`);
         break;
     }
+}
 
-    const entryToRemove = entries[sumIndex];
-    const sum = curNum + entryToRemove.num;
-    entries.splice(sumIndex, 1);
+if (badNum === undefined) {
+    throw new Error('Part 1 solution not found');
+}
 
-    for (const entry of entries) {
-        for (const [otherNum, curSum] of entry.sums.entries()) {
-            if (otherNum === curNum && curSum === sum) {
-                entry.sums.delete(curSum);
+let found = false;
+for (let i = 0; i < input.length && !found; i++) {
+    console.log('finding contiguous set from index', { i });
+    let curSum = input[i];
+
+    for (let j = i + 1; j < input.length && curSum < badNum; j++) {
+        curSum += input[j];
+        if (curSum === badNum) {
+            console.log('found result', { curSum, badNum, i, j, start: input[i], end: input[j] });
+
+            let smallest = input[i];
+            let largest = input[i];
+
+            for (let k = i; k <= j; k++) {
+                const curNum = input[k];
+                smallest = Math.min(smallest, curNum);
+                largest = Math.max(largest, curNum);
             }
+
+            console.log('found min/max', { smallest, largest });
+
+            console.log(`Part 2: ${smallest + largest}`);
+
+            found = true;
+            break;
         }
-        entry.sums.set(sum, curNum);
     }
 }
