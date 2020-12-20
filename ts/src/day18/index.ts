32d56a5fe742affa0f8c853efce7b94ba6d7c164
Nick Marsh <nickmarsh@gmail.com>
2020-12-20 11:33:39 +1000

    Day 18 and 19 template

---
+++ ts/src/day18/index.ts
@@ -0,0 +1,4 @@
+import { readLines } from '../lib';
+
+const input = readLines('src/day18/sample.txt');
+console.log('input', input);
---
+++ ts/src/day19/index.ts
@@ -0,0 +1,4 @@
+import { readLines } from '../lib';
+
+const input = readLines('src/day19/sample.txt');
+console.log('input', input);
