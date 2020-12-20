1d640d372ffcff9a0471a6b8043932c214306481
Nick Marsh <nickmarsh@gmail.com>
2020-12-20 13:16:47 +1000

    Day 18 part 2

--- ts/src/day18/index.ts
+++ ts/src/day18/index.ts
@@ -3,7 +3,25 @@ import { readLines } from '../lib';
 const input = readLines('src/day18/input.txt');
 // console.log('input', input);
 
-const findNextStartIndex = (symbols: (string | number)[]): number => {
+const applyPrecedence = (symbols: (string | number)[], startIndex: number): number => {
+  // find first addition until the next brackets
+  for (let i = startIndex + 1; i < symbols.length - 1; i++) {
+    const curSymbol = symbols[i];
+    if (curSymbol === ')') {
+      break;
+    }
+
+    if (curSymbol === '+') {
+      // console.log(`Applying precedence and returning index ${i - 1}`);
+      return i - 1;
+    }
+  }
+
+  // if no additions, fall back to normal start index
+  return startIndex;
+}
+
+const findNextStartIndex = (symbols: (string | number)[], part2: boolean): number => {
   if (symbols.length <= 1) {
     throw new Error("Shouldn't get here");
   }
@@ -25,16 +43,16 @@ const findNextStartIndex = (symbols: (string | number)[]): number => {
         if (otherSymbol === '(') {
           break;
         } else if (otherSymbol === ')') {
-          return i + 1;
+          return part2 ? applyPrecedence(symbols, i + 1) : i + 1;
         }
       }
     }
   }
 
-  return 0;
+  return part2 ? applyPrecedence(symbols, 0) : 0;
 }
 
-const evaluateLine = (line: string): number => {
+const evaluateLine = (line: string, part2: boolean): number => {
   const symbols = line.split('').filter(s => !!s.trim()).map(s => {
     const val = parseInt(s, 10);
     return isNaN(val) ? s : val;
@@ -42,7 +60,7 @@ const evaluateLine = (line: string): number => {
   // console.log('symbols', symbols);
 
   while (symbols.length > 1) {
-    const startIndex = findNextStartIndex(symbols);
+    const startIndex = findNextStartIndex(symbols, part2);
     const symbol = symbols[startIndex];
     // console.log(`Processing ${symbol} at ${startIndex}`);
 
@@ -78,9 +96,12 @@ const evaluateLine = (line: string): number => {
   return symbols[0] as number;
 }
 
-let sum = 0;
+let sumPart1 = 0;
+let sumPart2 = 0;
 for (const line of input) {
-  sum += evaluateLine(line);
+  sumPart1 += evaluateLine(line, false);
+  sumPart2 += evaluateLine(line, true);
 }
 
-console.log(`Part 1: ${sum}`);
+console.log(`Part 1: ${sumPart1}`);
+console.log(`Part 2: ${sumPart2}`);
