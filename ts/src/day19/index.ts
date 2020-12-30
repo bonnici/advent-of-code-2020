b6b9c85b1c8b53209ec0a7ea2f9d554ed7857ecc
Nick Marsh <nickmarsh@gmail.com>
2020-12-30 17:18:27 +1000

    Day 19 part 1

--- ts/src/day19/index.ts
+++ ts/src/day19/index.ts
@@ -27,6 +27,22 @@ class Option {
       this.ruleIds = ruleIds;
     }
   }
+
+  public toRegexStr(): string {
+    if (this.letter) {
+      return this.letter;
+    } else if (this.ruleIds) {
+      return this.ruleIds.map(id => {
+        const rule = rules[id];
+        if (!rule) {
+          throw new Error(`Invalid rule ${id} in option`);
+        }
+        return rule.toRegexStr();
+      }).join('');
+    } else {
+      throw new Error(`Invalid option`);
+    }
+  }
 }
 
 // tslint:disable-next-line:max-classes-per-file
@@ -57,6 +73,10 @@ class Rule {
       throw new Error(`Invalid line: ${line}`);
     }
   }
+
+  public toRegexStr(): string {
+    return `(${this.options.map(o => o.toRegexStr()).join('|')})`;
+  }
 }
 
 for (const line of input[0]) {
@@ -66,3 +86,16 @@ for (const line of input[0]) {
 
 // console.log('rules', rules);
 
+const rule0RegexStr = `^${rules[0].toRegexStr()}$`;
+// console.log('rule 0 regex', rule0RegexStr);
+
+const rule0Regex = new RegExp(rule0RegexStr);
+
+let matches = 0;
+for (const line of input[1]) {
+  if (line.match(rule0Regex)) {
+    matches++;
+  }
+}
+
+console.log(`Part 1: ${matches}`);
