6f019b5e0cf069900c464c88e08f20a437b2b552
Nick Marsh <nickmarsh@gmail.com>
2020-12-30 17:51:11 +1000

    Day 19 part 2

--- ts/src/day19/index.ts
+++ ts/src/day19/index.ts
@@ -16,9 +16,9 @@ class Option {
       this.letter = 'b';
     } else {
       const ruleIds = str.trim().split(' ').map(n => parseInt(n, 10));
-      if (ruleIds.length < 1 || ruleIds.length > 3) {
-        throw new Error(`Invalid option: ${str}`);
-      }
+      // if (ruleIds.length < 1 || ruleIds.length > 3) {
+      //   throw new Error(`Invalid option: ${str}`);
+      // }
       ruleIds.forEach(id => {
         if (isNaN(id)) {
           throw new Error(`Invalid option: ${str}`);
@@ -48,7 +48,7 @@ class Option {
 // tslint:disable-next-line:max-classes-per-file
 class Rule {
   public index: number;
-  options: Option[];
+  public options: Option[];
 
   constructor(line: string) {
     const split = line.split(':');
@@ -65,6 +65,7 @@ class Rule {
 
     const options = split[1].split('|');
 
+    /*
     if (options.length === 1) {
       this.options = [new Option(options[0])];
     } else if (options.length === 2) {
@@ -72,10 +73,16 @@ class Rule {
     } else {
       throw new Error(`Invalid line: ${line}`);
     }
+    */
+    this.options = options.map(o => new Option(o));
   }
 
   public toRegexStr(): string {
-    return `(${this.options.map(o => o.toRegexStr()).join('|')})`;
+    if (this.options.length === 1) {
+      return this.options[0].toRegexStr();
+    } else {
+      return `(?:${this.options.map(o => o.toRegexStr()).join('|')})`;
+    }
   }
 }
 
@@ -88,6 +95,9 @@ for (const line of input[0]) {
 
 const rule0RegexStr = `^${rules[0].toRegexStr()}$`;
 // console.log('rule 0 regex', rule0RegexStr);
+// console.log('rule 1 regex', `^${rules[1].toRegexStr()}$`);
+// console.log('rule 2 regex', `^${rules[2].toRegexStr()}$`);
+// console.log('rule 4 regex', `^${rules[4].toRegexStr()}$`);
 
 const rule0Regex = new RegExp(rule0RegexStr);
 
@@ -99,3 +109,34 @@ for (const line of input[1]) {
 }
 
 console.log(`Part 1: ${matches}`);
+
+const rule0RegexesPt2 = [];
+for (let i = 0; i < 10; i++) {
+  console.log(`Generating regexes iteration ${i + 1}`);
+
+  let rule8Line = '8: 42';
+  for (let x = 0; x < i; x++) {
+    rule8Line = `${rule8Line} |${' 42'.repeat(x + 2)}`;
+  }
+  rules[8] = new Rule(rule8Line);
+
+  for (let j = 0; j < 10; j++) {
+    let rule11Line = '11: 42 31';
+    for (let x = 0; x < j; x++) {
+      rule11Line = `${rule11Line} |${' 42'.repeat(x + 2)}${' 31'.repeat(x + 2)}`;
+    }
+    rules[11] = new Rule(rule11Line);
+
+    const regexStr = `^${rules[0].toRegexStr()}$`;
+    rule0RegexesPt2.push(new RegExp(regexStr))
+  }
+}
+
+let matchesPt2 = 0;
+for (const line of input[1]) {
+  if (rule0RegexesPt2.some(regex => line.match(regex))) {
+    matchesPt2++;
+  }
+}
+
+console.log(`Part 2: ${matchesPt2}`);
