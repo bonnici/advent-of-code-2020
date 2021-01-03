64c67bbe0957c37eea5f6581075226e0e09b6c19
Nick Marsh <nickmarsh@gmail.com>
2021-01-03 14:55:54 +1000

    Day 23 part 2

--- ts/src/day23/index.ts
+++ ts/src/day23/index.ts
@@ -19,10 +19,10 @@ class CupGamePart1 {
 
   public play(moves: number) {
     for (let i = 0; i < moves; i++) {
-      game.move();
+      this.move();
     }
     console.log(`-- final --`);
-    game.printCups();
+    this.printCups();
   }
 
   public move() {
@@ -90,6 +90,174 @@ class CupGamePart1 {
   }
 }
 
+/*
 const game = new CupGamePart1(parsed);
 game.play(100);
-console.log(`Part 1: ${game.labels()}`)
+console.log(`Part 1: ${game.labels()}`);
+*/
+
+// tslint:disable-next-line:max-classes-per-file
+interface LinkedListCup {
+  cupNum: number;
+  nextCup: LinkedListCup | undefined;
+}
+
+// tslint:disable-next-line:max-classes-per-file
+class CupGamePart2 {
+  public cups: Map<number, LinkedListCup> = new Map();
+  public numCups: number = 0;
+  public currentCup: LinkedListCup | undefined = undefined;
+  public firstCup: LinkedListCup | undefined = undefined;
+  public curMove: number;
+
+  constructor(cups: number[], numCups: number) {
+    let lastCup: LinkedListCup | undefined;
+    for (const cupNum of cups) {
+      lastCup = this.addCup(cupNum, lastCup);
+    }
+
+    let nextCupNum = cups.length + 1;
+    while (this.numCups < numCups) {
+      lastCup = this.addCup(nextCupNum, lastCup);
+      nextCupNum++;
+    }
+    console.log('Finished adding cups');
+
+    // @ts-ignore
+    lastCup.nextCup = this.firstCup;
+
+    this.currentCup = this.firstCup;
+
+    this.curMove = 1;
+  }
+
+  public play(moves: number) {
+    for (let i = 0; i < moves; i++) {
+      this.move();
+    }
+    // console.log(`-- final --`);
+    // this.printCups();
+  }
+
+  public move() {
+    // console.log(`-- move ${this.curMove} --`);
+    // this.printCups();
+    if (this.curMove % 100000 === 0) {
+      console.log(`-- move ${this.curMove} --`);
+    }
+
+    const pickedUp = this.pickupCups();
+    // console.log(`pick up: ${pickedUp.map(c => c.cupNum).join(', ')}`);
+
+    const destinationCup = this.findDestinationCup(pickedUp);
+    // console.log(`destination: ${destinationCup.cupNum}`);
+
+    const tempCup = destinationCup.nextCup;
+    destinationCup.nextCup = pickedUp[0];
+    pickedUp[pickedUp.length - 1].nextCup = tempCup;
+
+    this.currentCup = this.currentCup?.nextCup;
+    this.curMove++;
+    // console.log();
+  }
+
+  public part1() {
+    const oneCup = this.findCup(1);
+    if (!oneCup) {
+      throw new Error('Could not find a 1');
+    }
+
+    const result = [];
+    let curCup = oneCup;
+    while (curCup.nextCup !== oneCup) {
+      result.push(curCup.nextCup);
+      curCup = curCup.nextCup as LinkedListCup;
+    }
+    return result.map(c => (c as LinkedListCup).cupNum).join('');
+  }
+
+  public part2() {
+    const oneCup = this.findCup(1);
+    if (!oneCup) {
+      throw new Error('Could not find a 1');
+    }
+
+    const after1 = (oneCup.nextCup as LinkedListCup);
+    const afterThat = (after1.nextCup as LinkedListCup);
+
+    console.log(`Cups after 1: ${after1.cupNum}, ${afterThat.cupNum}`)
+
+    return after1.cupNum * afterThat.cupNum;
+  }
+
+  public printCups() {
+    const cupLabels = [];
+    let cupToPrint: LinkedListCup = this.firstCup as LinkedListCup;
+    do {
+      cupLabels.push(cupToPrint.cupNum);
+      cupToPrint = cupToPrint.nextCup as LinkedListCup;
+    } while (cupToPrint !== this.firstCup)
+
+    console.log(`cups: ${cupLabels.map(c => c === this.currentCup?.cupNum ? `(${c})` : c).join(' ')}`);
+  }
+
+  private findCup(cupNum: number): LinkedListCup | undefined {
+    return this.cups.get(cupNum);
+  }
+
+  private pickupCups(): LinkedListCup[] {
+    const result = [];
+    let curCup = this.currentCup;
+    for (let i = 0; i < 3; i++) {
+      const nextCup = curCup?.nextCup as LinkedListCup;
+      result.push(nextCup);
+      curCup = nextCup;
+    }
+
+    (this.currentCup as LinkedListCup).nextCup = curCup?.nextCup;
+
+    return result;
+  }
+
+  private findDestinationCup(pickedUp: LinkedListCup[]): LinkedListCup {
+    let targetCupNum = (this.currentCup as LinkedListCup).cupNum;
+    let destinationCup;
+    do {
+      targetCupNum--;
+      if (targetCupNum < 1) {
+        targetCupNum = this.numCups;
+      }
+
+      if (pickedUp.findIndex(c => c.cupNum === targetCupNum) === -1) {
+        destinationCup = this.findCup(targetCupNum) as LinkedListCup;
+      }
+    } while (!destinationCup)
+
+    return destinationCup;
+  }
+
+  private addCup(cupNum: number, lastCup: LinkedListCup | undefined) {
+    const curCup: LinkedListCup = { cupNum, nextCup: undefined };
+
+    if (this.firstCup === undefined) {
+      this.firstCup = curCup;
+    }
+
+    if (lastCup) {
+      lastCup.nextCup = curCup;
+    }
+
+    this.cups.set(cupNum, curCup);
+    this.numCups++;
+
+    return curCup;
+  }
+}
+
+const gamePt1 = new CupGamePart2(parsed, parsed.length);
+gamePt1.play(100);
+console.log(`Part 1: ${gamePt1.part1()}`);
+
+const gamePt2 = new CupGamePart2(parsed, 1000000);
+gamePt2.play(10000000);
+console.log(`Part 2: ${gamePt2.part2()}`);
