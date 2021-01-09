909eafef8dc77ebbf0a6b292179955c15346b108
Nick Marsh <nickmarsh@gmail.com>
2021-01-09 17:01:26 +1000

    Day 25 part 1

--- ts/src/day25/index.ts
+++ ts/src/day25/index.ts
@@ -1,9 +1,66 @@
 import { readLines } from '../lib';
 
-const input = readLines('src/day25/sample.txt');
-console.log('input', input);
+const input = readLines('src/day25/input.txt');
+// console.log('input', input);
 
 const cardPublicKey = parseInt(input[0], 10);
 const doorPublicKey = parseInt(input[1], 10);
-console.log('cardPublicKey', cardPublicKey);
-console.log('doorPublicKey', doorPublicKey);
+// console.log('cardPublicKey', cardPublicKey);
+// console.log('doorPublicKey', doorPublicKey);
+
+const transformSubjectNumber = (subjectNumber: number, loopSize: number): number => {
+  let result = 1;
+  for (let i = 0; i < loopSize; i++) {
+    result = result * subjectNumber;
+    result = result % 20201227;
+  }
+  return result;
+}
+
+const transformSubjectNumberIterative = (subjectNumber: number, lastValue: number): number => {
+  let newValue = lastValue;
+  newValue *= subjectNumber;
+  newValue %= 20201227;
+  return newValue;
+}
+
+const MAX_LOOP_SIZE = 100000000;
+
+let cardLoopSize: number | undefined;
+let doorLoopSize: number | undefined;
+let value = 1;
+
+for (let curLoopSize = 1; curLoopSize <= MAX_LOOP_SIZE; curLoopSize++) {
+  if (curLoopSize % 100000 === 0) {
+    // console.log(`Checking loop size ${curLoopSize}`);
+  }
+
+  value = transformSubjectNumberIterative(7, value);
+
+  if (value === cardPublicKey) {
+    cardLoopSize = curLoopSize;
+  }
+  if (value === doorPublicKey) {
+    doorLoopSize = curLoopSize;
+  }
+
+  if (cardLoopSize && doorLoopSize) {
+    break;
+  }
+}
+
+if (cardLoopSize === undefined || doorLoopSize === undefined) {
+  throw new Error('Max loop size reached');
+}
+
+console.log('cardLoopSize', cardLoopSize);
+console.log('doorLoopSize', doorLoopSize);
+
+const encryptionKey = transformSubjectNumber(doorPublicKey, cardLoopSize);
+const encryptionKey2 = transformSubjectNumber(cardPublicKey, doorLoopSize);
+
+if (encryptionKey !== encryptionKey2) {
+  throw new Error('Invalid encryption key');
+}
+
+console.log(`Part 1: ${encryptionKey}`);
