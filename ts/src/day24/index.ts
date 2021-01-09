5921a113c5c25c2d181008342b801932b2a77318
Nick Marsh <nickmarsh@gmail.com>
2021-01-09 16:46:18 +1000

    Day 24 part 2

--- ts/src/day24/index.ts
+++ ts/src/day24/index.ts
@@ -3,21 +3,6 @@ import { readLines } from '../lib';
 const input = readLines('src/day24/input.txt');
 // console.log('input', input);
 
-/*
-let minRowDiff = 0;
-let maxRowDiff = 0;
-
-for (const line of input) {
-  const rowDiff = (line.match(/s/g) || []).length - (line.match(/n/g) || []).length;
-  // console.log('Row diff: ', rowDiff);
-
-  minRowDiff = Math.min(minRowDiff, rowDiff);
-  maxRowDiff = Math.max(maxRowDiff, rowDiff);
-}
-
-console.log(`Min row diff: ${minRowDiff}, max row diff: ${maxRowDiff}`);
-*/
-
 enum Dir {
   E,
   SE,
@@ -63,60 +48,135 @@ const extractDirs = (line: string): Dir[] => {
   return result;
 }
 
-const flipped = new Set<string>();
-for (const line of input) {
-  const dirs = extractDirs(line);
-  let row = 0;
-  let col = 0;
-  let onOddRow = false;
+class Hex {
+  public row: number;
+  public col: number;
 
-  for (const dir of dirs) {
-    switch (dir) {
-      case Dir.E:
-        col += 1;
-        break;
-      case Dir.SE:
-        row += 1;
-        if (onOddRow) {
-          col += 1;
-        }
-        onOddRow = !onOddRow;
-        break;
-      case Dir.SW:
-        row += 1;
-        if (!onOddRow) {
-          col -= 1;
-        }
-        onOddRow = !onOddRow;
-        break;
-      case Dir.W:
-        col -= 1;
-        break;
-      case Dir.NW:
-        row -= 1;
-        if (!onOddRow) {
-          col -= 1;
-        }
-        onOddRow = !onOddRow;
-        break;
-      case Dir.NE:
-        row -= 1;
-        if (onOddRow) {
-          col += 1;
-        }
-        onOddRow = !onOddRow;
-        break;
-    }
+  constructor(hexStr: string) {
+    const split = hexStr.split(',');
+    this.row = parseInt(split[0], 10);
+    this.col = parseInt(split[1], 10);
   }
 
-  const hex = `${row},${col}`;
-  // console.log(`flipping hex ${hex}`);
+  toString() {
+    return `${this.row},${this.col}`
+  }
 
-  if (flipped.has(hex)) {
-    flipped.delete(hex);
-  } else {
-    flipped.add(hex);
+  move(dir: Dir) {
+    const onOddRow = this.row % 2 !== 0;
+    switch (dir) {
+      case Dir.E:
+        this.col += 1;
+        break;
+      case Dir.SE:
+        this.row += 1;
+        if (!onOddRow) {
+          this.col += 1;
+        }
+        break;
+      case Dir.SW:
+        this.row += 1;
+        if (onOddRow) {
+          this.col -= 1;
+        }
+        break;
+      case Dir.W:
+        this.col -= 1;
+        break;
+      case Dir.NW:
+        this.row -= 1;
+        if (onOddRow) {
+          this.col -= 1;
+        }
+        break;
+      case Dir.NE:
+        this.row -= 1;
+        if (!onOddRow) {
+          this.col += 1;
+        }
+        break;
+      default:
+        throw new Error('Invalid direction');
+    }
   }
 }
 
-console.log(`Part 1: ${flipped.size}`)
+let flipped = new Set<string>();
+for (const line of input) {
+  const dirs = extractDirs(line);
+  const hex = new Hex('0,0');
+
+  for (const dir of dirs) {
+    hex.move(dir);
+  }
+
+  const hexStr = hex.toString();
+  // console.log(`flipping hex ${hexStr}`);
+
+  if (flipped.has(hexStr)) {
+    flipped.delete(hexStr);
+  } else {
+    flipped.add(hexStr);
+  }
+}
+
+console.log(`Part 1: ${flipped.size}`);
+
+const blackNeighbour = (hexStr: string, dir: Dir, curFlipped: Set<string>): boolean => {
+  const hex = new Hex(hexStr);
+  if (dir !== undefined) {
+    hex.move(dir);
+  }
+  return curFlipped.has(hex.toString());
+}
+
+const evaluate = (hexStr: string, curFlipped: Set<string>, newFlipped: Set<string>, evaluated: Set<string>, dir: Dir | undefined) => {
+  const hex = new Hex(hexStr);
+  if (dir !== undefined) {
+    hex.move(dir);
+  }
+
+  if (evaluated.has(hex.toString())) {
+    return;
+  }
+
+  let surroundingCount = 0;
+  if (blackNeighbour(hex.toString(), Dir.E, curFlipped)) { surroundingCount++; }
+  if (blackNeighbour(hex.toString(), Dir.SE, curFlipped)) { surroundingCount++; }
+  if (blackNeighbour(hex.toString(), Dir.SW, curFlipped)) { surroundingCount++; }
+  if (blackNeighbour(hex.toString(), Dir.W, curFlipped)) { surroundingCount++; }
+  if (blackNeighbour(hex.toString(), Dir.NW, curFlipped)) { surroundingCount++; }
+  if (blackNeighbour(hex.toString(), Dir.NE, curFlipped)) { surroundingCount++; }
+
+  if (curFlipped.has(hex.toString())) {
+    if (surroundingCount === 0 || surroundingCount > 2 ) {
+      newFlipped.delete(hex.toString());
+    }
+  } else {
+    if (surroundingCount === 2) {
+      newFlipped.add(hex.toString());
+    }
+  }
+
+  evaluated.add(hex.toString());
+}
+
+for (let day = 1; day <= 100; day++) {
+  const newFlipped = new Set(flipped);
+  const evaluated = new Set<string>();
+
+  for (const hex of flipped.values()) {
+    evaluate(hex, flipped, newFlipped, evaluated, undefined);
+    evaluate(hex, flipped, newFlipped, evaluated, Dir.E);
+    evaluate(hex, flipped, newFlipped, evaluated, Dir.SE);
+    evaluate(hex, flipped, newFlipped, evaluated, Dir.SW);
+    evaluate(hex, flipped, newFlipped, evaluated, Dir.W);
+    evaluate(hex, flipped, newFlipped, evaluated, Dir.NW);
+    evaluate(hex, flipped, newFlipped, evaluated, Dir.NE);
+  }
+
+  flipped = newFlipped;
+  console.log(`Day ${day}: ${flipped.size}`);
+}
+
+console.log(`Part 2: ${flipped.size}`);
--- ts/src/day25/index.ts
+++ ts/src/day25/index.ts
@@ -2,3 +2,8 @@ import { readLines } from '../lib';
 
 const input = readLines('src/day25/sample.txt');
 console.log('input', input);
+
+const cardPublicKey = parseInt(input[0], 10);
+const doorPublicKey = parseInt(input[1], 10);
+console.log('cardPublicKey', cardPublicKey);
+console.log('doorPublicKey', doorPublicKey);
